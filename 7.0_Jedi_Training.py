413a9718fea84d7f993b58bf8890e0159b34e35e
Marc Hermon <hermonm@urbandaleschools.com>
2019-05-14 12:48:26 -0500

    Update and rename 7.0_Take_Home_Test.py to 7.0_Jedi_Training.py

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,6 @@
-'''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+#Sign your name:________________
 
+'''
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
