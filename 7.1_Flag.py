74f5e7bffe99c3dd72ca479c195c5b1a3ec043cb
Marc Hermon <hermonm@urbandaleschools.com>
2021-11-09 06:54:43 -0600

    Update 7.1_Flag.py

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -3,9 +3,10 @@ FLAG PROJECT
 ---------------
 Make your flag 260 pixels tall
 Use the scaling image on the website to determine other dimensions
-The hexadecimal colors for the official flag are red:#BF0A30 and blue:#002868
+The hexadecimal colors for the official flag are red: #BF0A30 and blue: #002868
 Title the window, "The Stars and Stripes"
-I used a draw_text command and used 20 pt. asterisks for the stars.
+You can use a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
+CHALLENGE: Can you make the entire flag parametrically? This means if I change the hoist to 520px the flag will resize accordingly.
 '''
